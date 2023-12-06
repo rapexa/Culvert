194886de9a6df7d764b026ab07ae03a832342bd1
rapexa <rapexamrs@gmail.com>
2023-12-07 00:04:43 +0330

    add app/calculate route to urls

--- Culvert/web/urls.py
+++ Culvert/web/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('auth/register', views.register, name='register'),
     path('auth/login', views.login, name='login'),
     path('auth/whoami', views.whoami, name='whoami'),
+    path('app/calculate', views.Calculate, name='Calculate'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
