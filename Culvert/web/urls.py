3258cbd0d810a34e7dc472bc83aa7317f8734d0c
rapexa <rapexamrs@gmail.com>
2024-09-01 01:35:12 +0430

    add new route

--- Culvert/web/urls.py
+++ Culvert/web/urls.py
@@ -7,11 +7,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
     path('auth/register', views.register, name='register'),
     path('auth/login', views.login, name='login'),
     path('auth/whoami', views.whoami, name='whoami'),
     path('app/calculate', views.Calculate, name='Calculate'),
+    #path('outputs', views.warpFile, name='warpFile'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
